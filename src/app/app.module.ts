9e04439915e581107688e4bb3f022cb9574cc0f3
lcyrillo <lucascyrillo@gmail.com>
2021-05-30 22:21:11 -0300

    create nav component and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { NavigationModule } from './navigation/navigation.module';
+
+import { AppRoutingModule } from './app.routes';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NavigationModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
